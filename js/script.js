65a8cc26efd89c653a6e6a2019a4c489605a7ed3
kushagra-ag <kushag44@yahoo.com>
2020-02-16 22:41:20 +0530

    Functionality

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 ;import { captions } from './captions.js';
+//import { para } from './extra.js';
 
 $(document).ready(function() {
 
@@ -6,9 +7,7 @@ $(document).ready(function() {
 	let containers = document.getElementsByClassName('content__container');
 	let nav = (document.querySelector('.navbar')).getBoundingClientRect();
 	let home = document.querySelector('.homepage');
-	containers[0].removeAttribute('style');
-	let ab = document.querySelector('.abcd');
-	console.log(ab);
+	containers[1].removeAttribute('style');
 	
 
 	[...containers].forEach((item)=>{
@@ -24,7 +23,13 @@ function showChapters()
 	let home = document.querySelector('.homepage');
 
 	// location.hash = "template";
-	// history.pushState(null,null,null);
+	history.pushState({
+		h_chapter: 0,
+		h_page: 0,
+		t_chapter: 1,
+		t_page: 1
+	},null,`chapter=1/page=1`);
+
 	setTimeout(function()
 	{
 		ch.style.display = "block";
@@ -34,58 +39,173 @@ function showChapters()
 
 /*-----Function for card swipe animations-----*/
 
-function pageChange()
+function pageChange(e)
 {
-	let elem = event.currentTarget;
-	let chapter = elem.dataset.chapter;
-	let page = elem.dataset.page;
-	let dir = elem.dataset.dir;
+	console.log(e.state)
 
-	let host = document.querySelector(`.content__container[data-chapter='${chapter}'][data-page='${page}']`);
 	
-	if(dir=="up")
+	
+	let elem = event.currentTarget;
+	let id = parseInt(elem.dataset.id);
+	let chapter = parseInt(elem.dataset.chapter);
+	let page = parseInt(elem.dataset.page);
+	let dir = elem.dataset.dir;
+	let prev,cur,next;
+
+	//page++;
+
+	console.log(id);
+	console.log(page);
+
+	if(id==1)
 	{
-		page--;
+		if(dir=="up")
+		{
+			
+			cur = page;
+			next = page-1;
+		}
+		else
+		{
+			next = page+1;
+			cur = page;
+			
+		}
+
+		if(next==0||next==4)
+			return;
+
+		let target = {
+			chapter: chapter,
+			page: next,
+		}
+
+		let host = {
+			chapter: chapter,
+			page: cur,
+		}
+
+		showPage(host,target,dir,id);
+
+		
 	}
+
 	else
 	{
-		page++;
-	}
-	
-	if(page==0||page==3)
-		return;
+		if(e.state)
+		{
 
-	let target = document.querySelector(`.content__container[data-chapter='${chapter}'][data-page='${page}']`);
-	let image = document.querySelector('.chapter__image');
-	let caption = document.querySelector('.caption');
-
-	image.classList.add('scale');
-	caption.classList.add('scale');
-
-	setTimeout(function(){
-		image.src = `./images/img${page}.png`;
-		caption.innerHTML = captions.key(page-1);
-		image.classList.remove('scale');
-		caption.classList.remove('scale');
-	},1000);
-
-	if(dir=="down")
-	{
-		
-		host.style.position = "absolute";
-		host.id = 'pageUp';
-		target.style.display = "block";
-	}
-
-	if(dir=="up")
-	{
-		target.id = 'pageDown';
+		}
 	}
 	
 
 }
 
+function showPage(h,t,dir,id)
+{
+
+	// console.log(h);
+	// console.log(t);
+
+	let host = document.querySelector(`.y[data-id='${id}']`);
+	//let host = document.getElementById(id);
+	console.log(host);
+	let dest = document.querySelector(`.y[data-id='${(id+1)%3}']`);
+	console.log(t);
+
+	import(`../modules/chapter${t.chapter}/page${t.page}.js`).then(
+		({d}) => {
+
+		//append imported module on selected id
+		//console.log(d);
+		dest.appendChild(d);
+
+
+
+		if(dir=="down")
+		{
+
+			host.style.position = "absolute";
+			host.id = 'pageUp';										// importedNode == target
+			d.style.display = "block";
+		}
+
+		if(dir=="up")
+		{
+			d.id = 'pageDown';
+		}
+
+		// dest.dataset.id = "2";
+		// host.dataset.id = "1";
+
+		bwd = document.querySelector(`.y[data-id='${(id+2)%3}']`);
+
+		// bwd.dataset.class = "up";
+		bwd.dataset.class = "behind";
+		//host.dataset.class = "current";
+	});
+
+
+
+
+
+
+	//let target = document.querySelector(`.content__container[data-chapter='${t.chapter}'][data-page='${t.page}']`);
+	let image = document.querySelector('.chapter__image');
+	let caption = document.querySelector('.caption');
+
+	history.pushState({
+		h_chapter: h.chapter,
+		h_page: h.page,
+		t_chapter: t.chapter,
+		t_page: t.page,
+	},null,`../chapter=${t.chapter}/page=${t.page}`);
+
+	image.classList.add('scale');
+	caption.classList.add('scale');
+
+	setTimeout(function(){
+		image.src = `../images/img${t.page}.png`;
+		caption.innerHTML = captions.key(t.page-1);
+		image.classList.remove('scale');
+		caption.classList.remove('scale');
+	},1000);
+
+	
+
+	
+
+
+	
+}
+
+
+function checkState(e)
+{
+	console.log(e.state);
+
+	if(e.state)
+	{
+
+		let target = {
+			chapter: e.state.t_chapter,
+			page: e.state.t_page
+		}
+
+		let host = {
+			chapter: e.state.h_chapter,
+			page: e.state.h_page
+		}
+
+		let dir = "up";
+
+		showPage(host,target,dir);
+	}
+}
+
 /*-----Attaching event listeners-----*/
+
+window.onpopstate = checkState;
 
 [...document.getElementsByClassName('learn')].forEach((elem) => {
 	elem.addEventListener('click', showChapters);
---
+++ modules/chapter1/page2.js
@@ -0,0 +1,6 @@
+var para = document.createElement("p");
+var node = document.createTextNode("This is new.");
+export var d = document.createElement("div");
+
+para.appendChild(node);
+d.appendChild(para);
